95922af77a69f65d2971ed2e87ae00818531d930
ligas10105 <ligass22@yahoo.com>
2023-03-30 17:42:03 +0200

    Adding palette movement using the keyboard.

--- src/Palette.cpp
+++ src/Palette.cpp
@@ -4,47 +4,46 @@
 
 #include "Palette.h"
 
-Palette :: Palette (float t_X, float t_Y) {
+Palette::Palette(float t_X, float t_Y) {
 
     shape.setPosition(t_X, t_Y);
-    shape.setSize({paletteWidth, paletteHeight });
-    shape.setFillColor(Color :: Red);
-    shape.setOrigin(this -> paletteWidth/2, this -> paletteHeight/2);
-
+    shape.setSize({paletteWidth, paletteHeight});
+    shape.setFillColor(Color::Red);
+    shape.setOrigin(this->paletteWidth / 2, this->paletteHeight / 2);
 
 
 }
 
-void Palette :: draw (RenderTarget& target, RenderStates state) const{
-    target.draw(this -> shape, state);
+void Palette::draw(RenderTarget &target, RenderStates state) const {
+    target.draw(this->shape, state);
 }
 
-void Palette :: update(){
-    this -> shape.move(velocity);
-    if (this -> left() < 0)
-    {
-        velocity.x = paletteVelocity;
-    }
-    else if (this -> right() > 800){
+void Palette::update() {
+
+
+    this->shape.move(velocity);
+    if (Keyboard::isKeyPressed(Keyboard::Key::Left) && this->left() > 0) {
         velocity.x = -paletteVelocity;
-
+    } else if (Keyboard::isKeyPressed(Keyboard::Key::Right) && this->right() < 800) {
+        velocity.x = paletteVelocity;
+    } else {
+        velocity.x = 0;
     }
-
-
 }
 
-float Palette :: left(){
-    return this -> shape.getPosition().x - shape.getSize().x / 2.f;
+
+float Palette::left() {
+    return this->shape.getPosition().x - shape.getSize().x / 2.f;
 }
 
-float Palette :: right(){
-    return this -> shape.getPosition().x + shape.getSize().x / 2.f;
+float Palette::right() {
+    return this->shape.getPosition().x + shape.getSize().x / 2.f;
 }
 
-float Palette :: top(){
-    return this -> shape.getPosition().y - shape.getSize().y / 2.f;
+float Palette::top() {
+    return this->shape.getPosition().y - shape.getSize().y / 2.f;
 }
 
-float Palette :: bottom(){
-    return this -> shape.getPosition().y + shape.getSize().y / 2.f;
+float Palette::bottom() {
+    return this->shape.getPosition().y + shape.getSize().y / 2.f;
 }
